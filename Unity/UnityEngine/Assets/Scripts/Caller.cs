85a3d19a7a4e767ed6fe0b53dfc7cda260501333
DeathWolf998 <sprad.jake@gmail.com>
2026-02-14 14:57:14 -0700

    Unity: Added Screenshots and Loop
    Added screenshots to show my program working and make the program working.

--- Unity/UnityEngine/Assets/Scripts/Caller.cs
+++ Unity/UnityEngine/Assets/Scripts/Caller.cs
@@ -3,11 +3,24 @@ using UnityEngine;
 public class Caller : MonoBehaviour
 {
     [SerializeField]
-    Receiver knownReceiver;
+    private Receiver knownReceiver;
+    private float timer = 5.0f;
 
     void Start()
     {
         print("Hello, darkness my old friend");
         print(knownReceiver.OnCalled());
+    }
+
+    private void Update()
+    {
+        if(timer <= 0)
+        {
+            timer = 5.0f;
+            print("Hello, darkness my old friend");
+            print(knownReceiver.OnCalled());
+        }
+        timer -= Time.deltaTime;
+
     }
 }
